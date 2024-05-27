d1c91b51d60bb95b929dac0824c2560d73ce0eda
Darya Surikova <d9041261928@gmail.com>
2024-05-27 11:36:26 +0200

    first try deserialize

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Serialize, Deserialize};
+use serde_yaml;
+#[derive(Serialize, Deserialize)]
+#[derive(Debug)]
+pub struct Contents
+{
+    pub id: String,
+    pub contents: Vec<u8>,
+}
+
+fn main() -> Result<(), serde_yaml::Error> {
+    let yaml = "
+      id: animal_record
+      contents:
+      - 0xca
+      - 0xfe
+      - 0xba
+      - 0xbe
+    ";
+
+    let deserialized_content = serde_yaml::from_str::<Contents>(&yaml)?;
+    println!("{:?}", deserialized_content);
+    Ok(())
 }

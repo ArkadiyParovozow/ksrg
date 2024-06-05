91e56434ca71a172d41a82d33121ebe19ff9b9a2
Darya Surikova <d9041261928@gmail.com>
2024-06-05 16:01:22 +0200

    change visit_seq function to deserialize extreme examples, add tests

--- src/contents.rs
+++ src/contents.rs
@@ -18,7 +18,7 @@ where
     impl<'de> de::Visitor<'de> for ContentsVisitor {
         type Value = Vec<u8>;
 
-        fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
             formatter.write_str("string or byte array")
         }
 
@@ -41,8 +41,26 @@ where
             A: de::SeqAccess<'de>,
         {
             let mut vec = Vec::with_capacity(seq.size_hint().unwrap_or(0));
-            while let Some(i) = seq.next_element()? {
-                vec.push(i);
+            while let Some(elem) = seq.next_element::<serde_yaml::Value>()? {
+                match elem {
+                    serde_yaml::Value::String(s) => {
+                        // Interpreting the string as bytes
+                        let bytes = if s.starts_with("0x") {
+                            s.as_bytes().to_owned()
+                        } else {
+                            s.into_bytes()
+                        };
+                        vec.extend(bytes);
+                    }
+                    serde_yaml::Value::Number(n) => {
+                        if let Some(byte) = n.as_u64() {
+                            vec.push(byte as u8);
+                        } else {
+                            return Err(de::Error::custom("Invalid number"));
+                        }
+                    }
+                    _ => return Err(de::Error::custom("Invalid type")),
+                }
             }
             Ok(vec)
         }
@@ -99,5 +117,41 @@ mod tests {
 
     let deserialized_content = serde_yaml::from_str::<Contents>(&yaml);
     assert!(!deserialized_content.is_ok());
+    }
+#[test]
+    fn string_and_number_case() {
+        let yaml = "
+        id: magic3
+        contents: [CAFE, 0, BABE]
+    ";
+
+    let deserialized_content = serde_yaml::from_str::<Contents>(&yaml);
+    assert!(
+        matches!(deserialized_content, Ok(content) if content.contents == vec![67, 65, 70, 69, 0, 66, 65, 66, 69])
+    );
+    }
+#[test]
+    fn byte_and_number_case() {
+        let yaml = "
+        id: magic4
+        contents: [foo, 0, A, 0xa, 42]
+    ";
+
+    let deserialized_content = serde_yaml::from_str::<Contents>(&yaml);
+    assert!(
+        matches!(deserialized_content, Ok(content) if content.contents == vec![102, 111, 111, 0, 65, 10, 42])
+    );
+    }
+#[test]
+    fn extreme_example() {
+        let yaml = "
+        id: magic5
+        contents: [1, 0x55, '▒,3', 3]
+    ";
+
+    let deserialized_content = serde_yaml::from_str::<Contents>(&yaml);
+    assert!(
+        matches!(deserialized_content, Ok(content) if content.contents == vec![1, 85, 226, 150, 146, 44, 51, 3])
+    );
     }
 }

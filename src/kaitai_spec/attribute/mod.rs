821d58d00a97347b3e8fd477c0f116d2efd10a7d
Darya Surikova <d9041261928@gmail.com>
2024-06-09 20:22:29 +0200

    try to do hierarchy

---
+++ src/kaitai_spec/attribute/mod.rs
@@ -0,0 +1,30 @@
+use serde::{Deserialize, Deserializer};
+
+pub enum AttributeType {
+}
+
+struct Attribute {
+    id: String,
+    doc: Option<String>,
+    type_: AttributeType,
+}
+
+impl<'de> Deserialize<'de> for Attribute {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+    {
+        struct Visitor;
+
+        impl<'de> serde::de::Visitor<'de> for Visitor {
+            type Value = Attribute;
+
+            fn visit_map<A>(self, map: A) -> Result<Self::Value, A::Error>
+                where
+                    A: serde::de::MapAccess<'de>, {
+                todo!()
+            }
+        }
+    }
+}
+

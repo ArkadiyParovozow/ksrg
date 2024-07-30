a41cb4550244a24c4ceb04c6d06dcbd7a4dd76e5
MrJonatans <145282159+MrJonatans@users.noreply.github.com>
2024-07-30 18:45:06 +0300

    create common_types + signed/unsigned deserialization

---
+++ src/kaitai_spec/attribute/integers.rs
@@ -0,0 +1,29 @@
+use super::common_types::IntType;
+pub fn process<'de, A>(
+    mut common_keys: super::CommonKeys,
+    mut map: A,
+) -> Result<super::Attribute, A::Error>
+where
+    A: serde::de::MapAccess<'de>,
+{
+    while let Some(key) = map.next_key::<&str>()? {
+        if common_keys.process(key, &mut map)? {
+            continue;
+        }
+        return Err(serde::de::Error::unknown_field(key, &["nothing expected"]));
+    }
+    let type_unchecked: String = common_keys
+        .type_
+        .ok_or_else(|| serde::de::Error::missing_field("type"))?;
+
+    let type_: IntType = super::common_types::type_parse::<A>(type_unchecked)?;
+
+    Ok(super::Attribute {
+        id: common_keys
+            .id
+            .ok_or_else(|| serde::de::Error::missing_field("id"))?,
+        doc: common_keys.doc,
+        doc_ref: common_keys.doc_ref,
+        type_:super::AttributeType::Integer(type_),
+    })
+}

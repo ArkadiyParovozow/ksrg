c2fd11d9c87611e7505c654032909ade6680753b
MrJonatans <145282159+MrJonatans@users.noreply.github.com>
2024-08-04 16:42:12 +0300

    refactor enumeration mod

--- src/kaitai_spec/attribute/enumeration.rs
+++ src/kaitai_spec/attribute/enumeration.rs
@@ -1,5 +1,6 @@
 use super::{Attribute, ContextNoContents, ContextTypeAttributes};
 use either::Either;
+use serde::de::Error;
 use std::collections::HashMap;
 
 #[derive(Debug, PartialEq)]
@@ -14,38 +15,47 @@ pub fn try_build<'de, A>(
 where
     A: serde::de::MapAccess<'de>,
 {
-    match context.type_attributes.is_some() {
-        true => return Either::Left(build_attribute::<A>(context)),
-        false => {
+    let enumeration = match context.type_attributes {
+        Some(Either::Left(enumeration)) => enumeration,
+        Some(Either::Right(value)) => {
             return Either::Right(ContextTypeAttributes {
                 string_keys: context.string_keys,
                 size: context.size,
                 type_: context.type_,
-                type_attributes: context.type_attributes.unwrap().right(),
+                type_attributes: Some(value),
             })
         }
-    }
+        None => {
+            return Either::Right(ContextTypeAttributes {
+                string_keys: context.string_keys,
+                size: context.size,
+                type_: context.type_,
+                type_attributes: None,
+            })
+        }
+    };
+    return Either::Left(build_attribute::<A>(context.string_keys, enumeration.0, context.type_));
 }
 
-fn build_attribute<'de, A>(context: ContextNoContents) -> Result<Attribute, A::Error>
+fn build_attribute<'de, A>(
+    mut keys: HashMap<&str, String>,
+    name: String,
+    type_unchecked: Option<String>
+) -> Result<Attribute, A::Error>
 where
     A: serde::de::MapAccess<'de>,
 {
-    let name: String = context.type_attributes.unwrap().left().unwrap().0;
-    let mut keys: HashMap<&str, String> = context.string_keys.clone();
     let id: String = keys
         .remove("id")
-        .ok_or_else(|| serde::de::Error::missing_field("type"))?;
+        .ok_or_else(|| Error::missing_field("type"))?;
     let doc: Option<String> = keys.remove("doc");
     let doc_ref: Option<String> = keys.remove("doc_ref");
     for key in keys.into_keys() {
-        return Err(serde::de::Error::unknown_field(key, &["id", "doc", "type"]));
+        return Err(Error::unknown_field(key, &["id", "doc", "type"]));
     }
 
-    let type_unchecked: String = context
-        .type_
-        .ok_or_else(|| serde::de::Error::missing_field("type"))?;
-    let type_: super::common::Integer = super::common::type_parse::<A>(type_unchecked)?;
+    let type_unchecked: String = type_unchecked.ok_or_else(|| Error::missing_field("type"))?;
+    let type_: super::common::Integer = super::common::type_parse::<A>(&type_unchecked)?;
 
     Ok(Attribute {
         id,
